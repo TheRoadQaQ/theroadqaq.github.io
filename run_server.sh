a84966751e3af5cf80f8e8334627e4c3a8f3e495
TheRoadQaQ <maluqaq@163.com>
2026-06-23 23:15:24 +0800

    init revise

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,3 @@
-bundle exec jekyll liveserve
+#!/bin/bash
+export PATH="/opt/homebrew/opt/ruby@3.1/bin:/opt/homebrew/lib/ruby/gems/3.1.0/bin:$PATH"
+bundle exec jekyll serve --host 127.0.0.1 --port 4000
